5f2ce379a3678d64dd181fb5877e05095327e84a
Nisrine Hamdouni <Nisrine@iMac-de-Nisrine.local>
2021-08-30 15:33:41 +0200

    Section 4

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,21 +1,22 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller","com.mycompany.dvdstore.repository.file","com.mycompany.dvdstore.service"})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class, args);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
