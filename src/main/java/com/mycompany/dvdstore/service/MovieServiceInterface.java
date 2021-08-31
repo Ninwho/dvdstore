4cff43d954f8de0daffde693b536a2eaaa594485
Nisrine Hamdouni <Nisrine@iMac-de-Nisrine.local>
2021-08-31 15:22:13 +0200

    Section5

--- src/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java
@@ -2,8 +2,11 @@ package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.util.List;
+
 public interface MovieServiceInterface {
 
     void registerMovie(Movie movie);
+    List<Movie> getMovieList();
 
 }
