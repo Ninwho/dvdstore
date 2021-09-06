82a1c2755f051f062185511c8e4e25cc9a585472
Nisrine Hamdouni <Nisrine@iMac-de-Nisrine.local>
2021-09-06 15:22:38 +0200

    Section 5: Modularité

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -14,8 +14,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         ApplicationContext context = SpringApplication.run(App.class, args);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
